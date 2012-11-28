b5cab63498f6292829f1874b0ee8dabd120436aa
sfkaos <win.raguini@gmail.com>
2012-11-27 18:35:07 -0800

    Initial Commit

---
+++ hAPI SDK/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  hAPI SDK
+//
+//  Created by Winfred Raguini on 11/27/12.
+//  Copyright (c) 2012 Medhelp International. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MHAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MHAppDelegate class]));
+    }
+}
---
+++ hAPI SDKTests/hAPI_SDKTests.m
@@ -0,0 +1,32 @@
+//
+//  hAPI_SDKTests.m
+//  hAPI SDKTests
+//
+//  Created by Winfred Raguini on 11/27/12.
+//  Copyright (c) 2012 Medhelp International. All rights reserved.
+//
+
+#import "hAPI_SDKTests.h"
+
+@implementation hAPI_SDKTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in hAPI SDKTests");
+}
+
+@end
