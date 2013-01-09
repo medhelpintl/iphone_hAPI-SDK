a476f1662475ca678e040e84415f0111b65cf65d
fishstix <fishstix@alum.mit.edu>
2013-01-09 14:54:15 -0800

    Updating Date Formats Adding Reachability

---
+++ hAPI SDK/NSDateFormatter+hAPI.h
@@ -0,0 +1,13 @@
+//
+//  NSDateFormatter+hAPI.h
+//  hAPI SDK
+//
+//  Created by Charles Fisher on 1/9/13.
+//  Copyright (c) 2013 Medhelp International. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSDateFormatter (hAPI)
+
+@end
---
+++ hAPI SDK/NSDateFormatter+hAPI.m
@@ -0,0 +1,13 @@
+//
+//  NSDateFormatter+hAPI.m
+//  hAPI SDK
+//
+//  Created by Charles Fisher on 1/9/13.
+//  Copyright (c) 2013 Medhelp International. All rights reserved.
+//
+
+#import "NSDateFormatter+hAPI.h"
+
+@implementation NSDateFormatter (hAPI)
+
+@end
